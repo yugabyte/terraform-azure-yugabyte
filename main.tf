07de41af24d82f078d38f0b3af14d33fca95eb8c
Ram Sri <ram@yugabyte.com>
2019-10-15 10:00:46 -0700

    Fix a typo in the network name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "YugaByte-Group" {
     }   
 }
 # Create virtual network
-resource "azurerm_virtual_network" "YugaByte-Netowrk" {
+resource "azurerm_virtual_network" "YugaByte-Network" {
     name                = "${var.prefix}${var.cluster_name}-VPC"
     address_space       = ["10.0.0.0/16"]
     location            = "${var.region_name}"
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "YugaByte-SubNet" {
     count                = "${var.subnet_count}"
     name                 = "${var.prefix}${var.cluster_name}-Subnet-${format("%d", count.index + 1)}"
     resource_group_name  = "${azurerm_resource_group.YugaByte-Group.name}"
-    virtual_network_name = "${azurerm_virtual_network.YugaByte-Netowrk.name}"
+    virtual_network_name = "${azurerm_virtual_network.YugaByte-Network.name}"
     address_prefix       = "10.0.${count.index+1}.0/24"
 }
 
